fbe441e1881faeb73f770aa19ab968d1ad15c21c
M. Zain Ul Abadin Malik <m.zainulabadeen@gmail.com>
2022-01-26 17:17:17 +0500

    no message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import * as path from 'path';
+import { diskStorage } from 'multer';
+
 
 @Controller()
 export class AppController {
@@ -8,5 +12,33 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':imgpath')
+seeUploadedFile(@Param('imgpath') image, @Res() res) {
+  return res.sendFile(image, { root: './ProfileImages' });
+}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './ProfileImages',
+      filename: (req, file, callBack) => {
+        const filename = path.parse(file.originalname).name + Date.now().toString();
+        const extension = path.parse(file.originalname).ext;
+        callBack(null, `${filename}${extension}`);
+      }
+    })
+  }))
+  uploadFile(@Res() res, @Req() req, @UploadedFile() file, @Body() body) {
+    var filename = req.file.filename;
+    console.log(filename);
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      data: file.path,
+      name: file.originalname,
+      Body: body.name,
+      body: body.rollno
+    });
   }
 }
